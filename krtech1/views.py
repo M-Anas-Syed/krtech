30f7917b5ea90ccc5162f3f40c0d97325dcedb14
“Anas <2436370s@student.gla.ac.uk>
2020-06-28 20:37:28 +0100

    half way through

--- krtech1/views.py
+++ krtech1/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Krtech says hey there partner!")
+    return render(request, 'krtech/index.html')
